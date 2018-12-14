b49d17d9309c45ac993b0f6916f603f1eca50abd
root <dcnocomment@gmail.com>
2018-12-14 02:25:28 +0000

    remove image_build

--- image_build/ros_demo/init.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/url : '\''ws:\/\/'\'' + this.url/url : '\''wss:\/\/'\'' + this.url + '\''\/websock'\'' + location.pathname/g' gzweb/http/client/gz3d.gui.js
-sed -i 's/modelUri = '\''http:\/\/'\'' + this.url + '\''\/'\'' + modelUri;/modelUri = '\''https:\/\/'\'' + this.url + '\''\/'\'' + modelUri;/g' gzweb/http/client/gz3d.gui.js
-cd gzweb; npm start 80 &
-/usr/sbin/sshd -D
